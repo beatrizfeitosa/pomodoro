585a0a66dde557cc404b3baa8c677522b8ad9b24
beatrizfeitosa <feitosatriz@gmail.com>
2026-03-29 21:11:19 -0300

    feat: menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,23 @@
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+export function Menu() {
+  return (
+    <>
+      <nav className={styles.menu}>
+        <a href='#' className={styles.menuLink}>
+          <HouseIcon />
+        </a>
+        <a href='#' className={styles.menuLink}>
+          <HistoryIcon />
+        </a>
+        <a href='#' className={styles.menuLink}>
+          <SettingsIcon />
+        </a>
+        <a href='#' className={styles.menuLink}>
+          <MoonIcon />
+        </a>
+      </nav>
+    </>
+  );
+}
